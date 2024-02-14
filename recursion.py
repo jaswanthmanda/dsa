01524ade0ba5493a15fc957ebbf9c51a32ea7b13
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2024-02-14 08:44:45 +0530

    recursion add.

--- recursion.py
+++ recursion.py
@@ -40,10 +40,58 @@ def recursive_palindrome(charti: str) -> bool:
         return True
     return recursive_palindrome(charti[1:-1]) if charti[0] == charti[-1] else False
 
+class SortArrayUsingRecursion:
+
+    def __init__(self) -> None:
+        pass
+
+    def insert_into_array(self, arr: list, item: int) -> list:
+        """Inserts item into list for sorting"""
+        if len(arr) == 1:
+            if item <= arr[0]:
+                arr.insert(0, item)
+            elif item >= arr[0]:
+                arr.append(item)
+            return arr
+        if item <= arr[-1]:
+            new_item = arr[-1]
+            arr = arr[:-1]
+            self.insert_into_array(arr, item)
+            arr.append(new_item)
+        else:
+            arr.append(item)
+
+        return arr
+
+
+    def sort_an_array_recu(self, arr: list) -> list:
+        """Sorts an array using recursion"""
+        # base case
+        if len(arr) == 0 or len(arr) == 1:
+            return arr
+
+        # base logic
+        item = arr[-1]
+        arr = arr[:-1]
+        arr = self.sort_an_array_recu(arr)
+        if item <= arr[0]:
+            arr.insert(0, item)
+        elif item >= arr[-1]:
+            arr.append(item)
+        else:
+            arr = self.insert_into_array(arr, item)
+        return arr
+
+
 # function calls
 # print(factorial(5))
 # print(fibonacci_num(5))
 # print_1_to_n(4)
 # print_n_to_1(25)
 # print(sum_of_numbers(25))
-print(recursive_palindrome("abbbba"))
+# print(recursive_palindrome("abbbba"))
+new_obj = SortArrayUsingRecursion()
+print(new_obj.sort_an_array_recu([2, 1, 4, 3, 5, 6]))
+print(new_obj.sort_an_array_recu([2]))
+print(new_obj.sort_an_array_recu([1]))
+print(new_obj.sort_an_array_recu([1, 2, 3, 4, 5, 6, 7]))
