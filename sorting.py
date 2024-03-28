20498c85206f616a083ba627bd592e482d1a8083
Jaswanth Manda <jaswanth@true-fan.in>
2024-03-28 13:32:31 +0530

    added intersection prob

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
-# merge two union sorted arr
+# merge union sorted arr
+# only one occurence of each element in the final list
 arr1 = [2, 10, 20, 20]
 arr2 = [3, 20, 40]
 
@@ -34,6 +35,33 @@ while ind2 < n:
         new_list.append(arr2[ind2])
     ind2 += 1
 
-print(new_list)
+print(new_list) # output: [2, 3, 10, 20, 40]
 
-# second one.
+# intersection of sorted lists
+# new list with single occurence of repeated elem
+arr1 = [1, 20, 20, 40, 60]
+arr2 = [2, 20, 20, 20, 40]
+
+ind1 = 0
+ind2 = 0
+
+m = len(arr1)
+n = len(arr2)
+
+inters_list = []
+
+while ind1 < m and ind2 < n:
+    if ind1 > 0 and arr1[ind1] == arr1[ind1-1]:
+        ind1 += 1
+    elif ind2 > 0 and arr2[ind2] == arr2[ind2-1]:
+        ind2 += 1
+    elif arr1[ind1] < arr2[ind2]:
+        ind1 += 1
+    elif arr2[ind2] < arr1[ind1]:
+        ind2 += 1
+    elif arr1[ind1] == arr2[ind2]:
+        inters_list.append(arr1[ind1])
+        ind1 += 1
+        ind2 += 1
+
+print(inters_list)  # output: [20, 40]
