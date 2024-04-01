b8412edbe04b3f64121dcbde42e80bd4ead80b95
Jaswanth Manda <jaswanth@true-fan.in>
2024-04-01 13:22:53 +0530

    added bubble sort, selection sort

--- sorting.py
+++ sorting.py
@@ -63,3 +63,46 @@ while ind1 < m and ind2 < n:
         ind2 += 1
 
 print(inters_list)  # output: [20, 40] (t.c: o(m+n))
+
+
+def bubble_sort(arr):
+    # Your code here
+    """
+    Compares with next element
+    """
+    swapped = True
+    while swapped is True:
+        swapped = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = True
+
+    return arr
+
+
+def selection_sort(arr):
+    # Your code here
+    pass
+
+# Test cases
+print(selection_sort([5, 3, 8, 1, 2]))  # Output: [1, 2, 3, 5, 8]
+print(selection_sort([9, 0, 6, 3, 2]))  # Output: [0, 2, 3, 6, 9]
+print(selection_sort([1, 2, 3, 4, 5]))  # Output: [1, 2, 3, 4, 5]
+print(selection_sort([]))              # Output: []
+print(selection_sort([5]))             # Output: [5]
+print(selection_sort([5, 5, 5]))       # Output: [5, 5, 5]
+print(selection_sort([1, 2, 3, 4, 5])) # Output: [1, 2, 3, 4, 5]
+print(selection_sort([5, 4, 3, 2, 1])) # Output: [1, 2, 3, 4, 5]
+print(selection_sort([5, 1, 2, 3, 4])) # Output: [1, 2, 3, 4, 5]
+
+
+
+
+# Test cases
+print(bubble_sort([5, 3, 8, 1, 2]))  # Output: [1, 2, 3, 5, 8]
+print(bubble_sort([9, 0, 6, 3, 2]))  # Output: [0, 2, 3, 6, 9]
+print(bubble_sort([1, 2, 3, 4, 5]))  # Output: [1, 2, 3, 4, 5]
+print(bubble_sort([]))  # Output: []
+print(bubble_sort([1]))  # Output: [1]
+print(bubble_sort([5, 5, 5]))  # Output: [5, 5, 5]
