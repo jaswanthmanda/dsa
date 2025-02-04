715050cfdb1ec8accc7994113f3c072b46e5859b
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2025-02-05 00:30:25 +0530

    added 3_count_number_of_anagrams

---
+++ sliding_window/fixed_no/3_count_number_of_anagrams.py
@@ -0,0 +1,104 @@
+"""
+1. Count no of anangrams basic example
+i/p string: for (check anagram)
+i/p string: forxxorfxdorf
+"""
+
+input_string = "forxxorfxdorf"
+check_anagram_string = "for"
+K = len(check_anagram_string)
+i = 0
+j = 0
+
+result = 0
+
+counter = {}
+match_counter = {}
+
+for char in check_anagram_string:
+    if char in match_counter:
+        match_counter[char] += 1
+    else:
+        match_counter[char] = 1
+
+# main logic
+while j < len(input_string):
+    if j - i + 1 < K:
+        if input_string[j] in counter:
+            counter[input_string[j]] += 1
+        else:
+            counter[input_string[j]] = 1
+        j += 1
+    elif j - i + 1 == K:
+        if input_string[j] in counter:
+            counter[input_string[j]] += 1
+        else:
+            counter[input_string[j]] = 1
+
+        if counter == match_counter:
+            result += 1
+
+        if counter[input_string[i]] - 1 > 0:
+            counter[input_string[i]] -= 1
+        else:
+            del counter[input_string[i]]
+
+        i += 1
+        j += 1
+
+print(result)
+print()
+print()
+
+
+"""
+Input:
+txt = aabaabaa
+pat = aaba
+Output: 4
+Explanation: aaba is present 4 times
+in txt.
+"""
+input_string = "aabaabaa"
+check_anagram_string = "aaba"
+K = len(check_anagram_string)
+i = 0
+j = 0
+
+result = 0
+
+counter = {}
+match_counter = {}
+
+for char in check_anagram_string:
+    if char in match_counter:
+        match_counter[char] += 1
+    else:
+        match_counter[char] = 1
+
+# main logic
+while j < len(input_string):
+    if j - i + 1 < K:
+        if input_string[j] in counter:
+            counter[input_string[j]] += 1
+        else:
+            counter[input_string[j]] = 1
+        j += 1
+    elif j - i + 1 == K:
+        if input_string[j] in counter:
+            counter[input_string[j]] += 1
+        else:
+            counter[input_string[j]] = 1
+
+        if counter == match_counter:
+            result += 1
+
+        if counter[input_string[i]] - 1 > 0:
+            counter[input_string[i]] -= 1
+        else:
+            del counter[input_string[i]]
+
+        i += 1
+        j += 1
+
+print(result)
