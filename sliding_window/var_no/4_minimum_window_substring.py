bab98abc64ebe871dbf3e12973e85fdb583aaa14
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2025-02-12 23:30:46 +0530

    updated and fixed 4) minimum window substring tc:O(n), sc:O(m)

--- sliding_window/var_no/4_minimum_window_substring.py
+++ sliding_window/var_no/4_minimum_window_substring.py
@@ -16,13 +16,16 @@ i = 0
 j = 0
 shortest = sys.maxsize
 ans = None
-while j < len(main_str):
+while i < len(main_str) or j < len(main_str):
     if len(char_counter) < len(conditional_str):
-        if main_str[j] in conditional_str and main_str[j] in char_counter:
-            char_counter[main_str[j]] += 1
-        elif main_str[j] in conditional_str and main_str[j] not in char_counter:
-            char_counter[main_str[j]] = 1
-        j += 1
+        if j < len(main_str):
+            if main_str[j] in conditional_str and main_str[j] in char_counter:
+                char_counter[main_str[j]] += 1
+            elif main_str[j] in conditional_str and main_str[j] not in char_counter:
+                char_counter[main_str[j]] = 1
+            j += 1
+        else:
+            break
     elif len(char_counter) == len(conditional_str):
         if shortest > j - i + 1:
             shortest = j - i + 1
@@ -36,10 +39,10 @@ while j < len(main_str):
                 char_counter[main_str[i]] -= 1
             elif (
                 main_str[i] in conditional_str
-                and main_str[i] not in char_counter
-                and char_counter - 1 == 0
+                and main_str[i] in char_counter
+                and char_counter[main_str[i]] - 1 == 0
             ):
-                char_counter[main_str[i]] -= 1
+                del char_counter[main_str[i]]
 
             i += 1
         else:
@@ -47,7 +50,23 @@ while j < len(main_str):
                 char_counter[main_str[j]] += 1
             elif main_str[j] in conditional_str and main_str[j] not in char_counter:
                 char_counter[main_str[j]] = 1
-
             j += 1
+    else:
+        if i < j:
+            if (
+                main_str[i] in conditional_str
+                and main_str[i] in char_counter
+                and char_counter[main_str[i]] - 1 > 0
+            ):
+                char_counter[main_str[i]] -= 1
+            elif (
+                main_str[i] in conditional_str
+                and main_str[i] in char_counter
+                and char_counter[main_str[i]] - 1 == 0
+            ):
+                del char_counter[main_str[i]]
+
+            i += 1
+
 
 print(ans)
