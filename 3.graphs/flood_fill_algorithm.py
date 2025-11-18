84fd8339e77be0447278b9678299b4607aaa891a
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2025-11-18 14:21:43 +0530

    added flood_fill_algo & number_of_islands (graphs)

--- 3.graphs/flood_fill_algorithm.py
+++ 3.graphs/flood_fill_algorithm.py
@@ -1,40 +1,92 @@
-# Number of islands
-"""
-Given a grid of size N x M (N is the number of rows and M is the number of columns in the grid)
-consisting of '0's (Water) and ‘1's(Land).
-Find the number of islands.
+from collections import deque
 
-An island is surrounded by water and is formed by
-connecting adjacent lands horizontally or
-vertically or diagonally i.e., in all 8 directions.
+# Flood fill algorithm
+"""
+An image is represented by a 2-D array of integers,
+each integer representing the pixel value of the image.
+Given a coordinate (sr, sc) representing the starting pixel
+(row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a flood fill, consider the starting pixel,
+plus any pixels connected 4-directionally to the starting pixel of
+the same colour as the starting pixel, plus any pixels connected 4-directionally
+to those pixels (also with the same colour as the starting pixel), and so on.
+Replace the colour of all of the aforementioned pixels with the newColor.
 """
 
 """
 Examples:
 
-Input: grid = [ ["1", "1", "1", "0", "1"], ["1", "0", "0", "0", "0"], ["1", "1", "1", "0", "1"], ["0", "0", "0", "1", "1"] ]
-Output: 2
+Input: image = [ [1, 1, 1], [1, 1, 0], [1, 0, 1] ], sr = 1, sc = 1, newColor = 2
+Output: [ [2, 2, 2], [2, 2, 0], [2, 0, 1] ]
 Explanation:
-This grid contains 2 islands. Each '1' represents a piece of land, and the islands are formed by connecting adjacent lands horizontally or vertically.
-Despite some islands having a common edge, they are considered separate
-islands because there is no land connectivity in any of the eight directions between them. Therefore, the grid contains 2 islands.
-
-Input: grid = [ ["1", "0", "0", "0", "1"], ["0", "1", "0", "1", "0"], ["0", "0", "1", "0", "0"], ["0", "1", "0", "1"," 0"] ]
-Output: 1
-Explanation:
-In the given grid, there's only one island as all the '1's are connected either horizontally,
-vertically, or diagonally, forming a single contiguous landmass surrounded by water on all sides.
+From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel),
+all pixels connected
+by a path of the same color as the starting pixel
+(i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2,
+because it is not 4-directionally connected to the starting pixel.
 
 
-Constraints:
-·  N == grid.length
-
-·  M == grid[i].length
-
-·  1 <= N, M <= 300
-
-·  grid[i][j] is '0' or '1'.
+Input: image = [ [0, 1, 0], [1, 1, 0], [0, 0, 1] ], sr = 2, sc = 2, newColor = 3
+Output: [ [0, 1, 0], [1, 1, 0], [0, 0, 3] ]
+Explanation: Starting from the pixel at position (2, 2) (i.e., the blue pixel), we flood fill all adjacent pixels that have the same color as the starting pixel. In this case, only the pixel at position (2, 2) itself is of the same color. So, only that pixel gets colored with the new color, resulting in the updated image.
 """
 
+"""
+Constraints:
+·  n == image.length
+
+·  m == image[i].length
+
+·  1 <= m, n <= 50
+
+·  0 <= image[i][j], color < 216
+
+·  0 <= sr < n
+
+·  0 <= sc < m
+"""
+
+
 class Solution:
-    def numIslands(self, grid):
+    def floodFill(self, image, sr, sc, newColor):
+        visited = set()
+        m = len(image)
+        n = len(image[0])
+        mewVal = image[sr][sc]
+        import copy
+
+        newPixel = copy.deepcopy(image)
+
+        q = deque([(sr, sc)])
+        visited.add((sr, sc))
+
+        while q:
+            node = q.popleft()
+            r = node[0]
+            c = node[1]
+            newPixel[r][c] = newColor
+
+            vals = [
+                (r - 1, c),
+                (r, c - 1),
+                (r+1, c),
+                (r, c + 1),
+            ]
+
+            for val in vals:
+                a, b = val
+                if 0 <= a < m and 0 <= b < n:
+                    if image[a][b] == mewVal and (a, b) not in visited:
+                        visited.add((a, b))
+                        q.append((a, b))
+
+        return newPixel
+
+
+s = Solution()
+k1 = s.floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2)
+k2 = s.floodFill([[0, 1, 0], [1, 1, 0], [0, 0, 1]], 2, 2, 3)
+print(k1)
+print(k2)
--- 3.graphs/number_of_islands.py
+++ 3.graphs/number_of_islands.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Number of islands
 """
 Given a grid of size N x M (N is the number of rows and M is the number of columns in the grid)
@@ -36,5 +38,57 @@ Constraints:
 ·  grid[i][j] is '0' or '1'.
 """
 
+
 class Solution:
     def numIslands(self, grid):
+        visited = set()
+        m = len(grid)
+        n = len(grid[0])
+        count = 0
+
+        def bfs(r, c, grid):
+            q = deque([(r, c)])
+            visited.add((r, c))
+
+            while q:
+                k = q.popleft()
+                row = k[0]
+                col = k[1]
+
+                for a in range(row - 1, row + 2):
+                    for b in range(col - 1, col + 2):
+                        if 0 <= a < m and 0 <= b < n:
+                            if grid[a][b] == "1" and (a, b) not in visited:
+                                visited.add((a, b))
+                                q.append((a, b))
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    bfs(i, j, grid)
+                    count += 1
+
+        return count
+
+
+s = Solution()
+k1 = s.numIslands(
+    [
+        ["1", "1", "1", "0", "1"],
+        ["1", "0", "0", "0", "0"],
+        ["1", "1", "1", "0", "1"],
+        ["0", "0", "0", "1", "1"],
+    ]
+)
+
+k2 = s.numIslands(
+    [
+        ["1", "0", "0", "0", "1"],
+        ["0", "1", "0", "1", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "1", "0", "1", " 0"],
+    ]
+)
+
+print(k1)
+print(k2)
