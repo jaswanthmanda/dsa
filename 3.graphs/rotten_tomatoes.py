4c4c55c0fa60c6e66d1405609c0aa138c98bbb99
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2025-11-19 09:18:35 +0530

    rotten_oranges primary approach not working

--- 3.graphs/rotten_tomatoes.py
+++ 3.graphs/rotten_tomatoes.py
@@ -29,3 +29,55 @@ Output: 4
 
 class Solution:
     def orangesRotting(self, grid):
+        visited = set()
+        m = len(grid)
+        n = len(grid[0])
+
+        def bfs(a, b):
+            q = deque([(a, b)])
+            visited.add((a, b))
+            minut = 0
+
+            while q:
+                c, d = q.popleft()
+
+                vals = [
+                    (c-1, d),
+                    (c+1, d),
+                    (c, d-1),
+                    (c, d+1),
+                ]
+
+                found = False
+
+                for val in vals:
+                    row = val[0]
+                    col = val[1]
+                    if 0 <= row < m and 0 <= col < n and grid[row][col] == 1 and (row, col) not in visited:
+                        grid[row][col] = 2
+                        visited.add((row, col))
+                        q.append((row, col))
+                        
+                        if not found:
+                            found = True
+                
+                if (c, d) != (a, b) and found:
+                    minut += 1
+            
+            return minut
+
+        minCounter = 501
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2 and (i, j) not in visited:
+                    counter = bfs(i, j)
+                    if counter != 0 and minCounter > counter:
+                        minCounter = counter
+        
+        for aa in range(m):
+            for ab in range(n):
+                if grid[aa][ab] == 1:
+                    return -1
+        
+        return minCounter
