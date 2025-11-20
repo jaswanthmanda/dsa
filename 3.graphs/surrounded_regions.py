afef5d19cc82069a75f43f6b3e21d6c698cec9ed
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2025-11-20 09:03:48 +0530

    surrounded regions solved (graphs)

--- 3.graphs/surrounded_regions.py
+++ 3.graphs/surrounded_regions.py
@@ -1,3 +1,6 @@
+import copy
+from collections import deque
+
 # Surrounded Regions
 """
 You are given a matrix mat of size N x M where each cell contains either 'O' or 'X'.
@@ -31,5 +34,91 @@ Constraints:
   mat[i][j] is 'X' or 'O'.
 """
 
+
 class Solution:
     def fill(self, mat):
+        m = len(mat)
+        n = len(mat[0])
+        visited = set()
+
+        newVal = copy.deepcopy(mat)
+
+        def bfs(row, col):
+            q = deque([(row, col)])
+            visited.add((row, col))
+            stack = []
+            touches_edge = False
+
+            while q:
+                r, c = q.popleft()
+                stack.append((r, c))
+
+                vals = [
+                    (r - 1, c),
+                    (r + 1, c),
+                    (r, c - 1),
+                    (r, c + 1),
+                ]
+
+                for val in vals:
+                    a, b = val[0], val[1]
+                    if (
+                        0 <= a < m
+                        and 0 <= b < n
+                        and mat[a][b] == "O"
+                        and (a, b) not in visited
+                    ):
+                        if a in [0, m - 1] or b in [0, n - 1]:
+                            touches_edge = True
+                        visited.add((a, b))
+                        q.append((a, b))
+
+            if touches_edge:
+                return []
+
+            return stack
+
+        def replaceVals(stacs):
+            if len(stacs) == 0:
+                return
+
+            while stacs:
+                c, d = stacs.pop()
+
+                newVal[c][d] = "X"
+
+        for i in range(m):
+            for j in range(n):
+                if (
+                    i not in [0, m - 1]
+                    and j not in [0, n - 1]
+                    and mat[i][j] == "O"
+                    and (i, j) not in visited
+                ):
+                    stac = bfs(i, j)
+                    replaceVals(stac)
+
+        return newVal
+
+
+sk = Solution()
+
+k1 = sk.fill(
+    [
+        ["X", "X", "X", "X"],
+        ["X", "O", "O", "X"],
+        ["X", "X", "O", "X"],
+        ["X", "O", "X", "X"],
+    ]
+)
+
+k2 = sk.fill(
+    [
+        ["X", "X", "X"],
+        ["X", "O", "X"],
+        ["X", "X", "X"],
+    ]
+)
+
+print(k1)
+print(k2)
