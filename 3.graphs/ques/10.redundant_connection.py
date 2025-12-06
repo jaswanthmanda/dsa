7bda476bd66ad32f8dcbaa013a1db1df0db33237
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2025-12-06 09:26:10 +0530

    code 1st base added

--- 3.graphs/ques/10.redundant_connection.py
+++ 3.graphs/ques/10.redundant_connection.py
@@ -32,10 +32,70 @@ Constraints:
 
 
 class Solution(object):
+    def dfs(self, start, parent, adjList):
+        self.visiting.add(start)
+
+        ans = None
+
+        for nei in adjList[start]:
+            print(parent, start, nei)
+            if nei not in self.visited and nei not in self.visiting:
+                ans = self.dfs(nei, start, adjList)
+            elif nei in self.visiting:
+                return (start, nei)
+
+        self.visiting.remove(start)
+        self.visited.add(start)
+
+        return ans
+
     def findRedundantConnection(self, edges):
         """
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+        # build adj list
+        maxNode = 0
 
-# chat url: https://chatgpt.com/share/6932d0ba-bb1c-800f-a919-80272305d3ec
+        for item in edges:
+            maxNode = max(maxNode, item[0])
+            maxNode = max(maxNode, item[1])
+
+        adjList = {i: [] for i in range(1, maxNode + 1)}
+
+        for item in edges:
+            adjList[item[0]].append(item[1])
+            adjList[item[1]].append(item[0])
+
+        print(adjList)
+
+        self.visited = set()
+        self.visiting = set()
+
+        ans = self.dfs(1, -1, adjList)
+
+        return [ans[0], ans[1]]
+
+
+s = Solution()
+
+k1 = s.findRedundantConnection(
+    [
+        [1, 2],
+        [1, 3],
+        [2, 3],
+    ]
+)
+
+k2 = s.findRedundantConnection(
+    [
+        [1, 2],
+        [2, 3],
+        [3, 4],
+        [1, 4],
+        [1, 5],
+    ]
+)
+
+print(k1)
+print(k2)
